c593c2f1e57c53810b87bbc8d7d70c320799dd60
Misty1636 <a3722041a@yahoo.com.tw>
2019-07-19 15:31:09 +0800

    update Day 3

---
+++ 03 - CSS Variables/js/all.js
@@ -0,0 +1,12 @@
+(function changeCss() {
+  const inputs = document.querySelectorAll('.option input');
+  function updateCSS() {
+    const unit = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, `${this.value}${unit}`);
+  }
+
+  inputs.forEach((item) => {
+    item.addEventListener('change', updateCSS);
+    item.addEventListener('mousemove', updateCSS);
+  });
+}());
